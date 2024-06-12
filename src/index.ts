82a00811476c5cba46ab99a376d2db433bd0f60b
Helpless <letalerv@gmail.com>
2024-06-12 20:20:45 +0300

    fix: fixed import and name tests folder in biome config

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,3 @@ export * from "./Types/PoeResponseType.js";
 export * from "./Types/TradeRequestBodyType.js";
 export * from "./Types/types.js";
 export * from "./constants.js";
-export * from "./utility/index.js";
