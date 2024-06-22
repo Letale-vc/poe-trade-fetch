0dc190da65a0f611a677a70ab6bb3a174b2e46e4
Helpless <letalerv@gmail.com>
2024-06-22 04:51:34 +0300

    refactor: Update PoeTradeFetchError to handle AxiosError<PoeError>

--- src/poeTradeFetchError.ts
+++ src/poeTradeFetchError.ts
@@ -1,9 +1,5 @@
 import { type AxiosError, isAxiosError } from "axios";
-import type {
-    ErrorCodesType,
-    ErrorMessagesType,
-    PoeErrorMessage,
-} from "./Types";
+import type { ErrorCodesType, ErrorMessagesType, PoeError } from "./Types";
 
 export class PoeTradeFetchError extends Error {
     public axiosError: AxiosError | undefined;
@@ -13,15 +9,17 @@ export class PoeTradeFetchError extends Error {
     public poeTradeApiErrorMessage: ErrorMessagesType | undefined;
     constructor(error: unknown) {
         super();
-        if (isAxiosError<PoeErrorMessage>(error)) {
+        if (isAxiosError<PoeError>(error)) {
             this.message = error.message;
             this.isAxiosError = true;
             this.axiosError = error;
             if (error.response) {
-                this.isPoeApiError = true;
-                this.poeTradeApiErrorCode = error.response.data.error.code;
-                this.poeTradeApiErrorMessage =
-                    error.response.data.error.message;
+                if (error.response.data.error) {
+                    this.isPoeApiError = true;
+                    this.poeTradeApiErrorCode = error.response.data.error.code;
+                    this.poeTradeApiErrorMessage =
+                        error.response.data.error.message;
+                }
             }
         } else if (error instanceof Error) {
             this.message = error.message;
