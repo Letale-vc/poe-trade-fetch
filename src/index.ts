6653174429db46b4a534bfb7804b201288920e89
Helpless <letalerv@gmail.com>
2025-03-14 17:25:59 +0200

    fix: update package.json exports for improved module resolution and add missing ExchangeResponse export

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export * from "./PoeTradeFetch.js";
 export * from "./Types/PoeErrorMessage.js";
 export * from "./Types/PoeResponse.js";
 export * from "./Types/TradeRequestBodyType.js";
+export * from "./Types/ExchangeResponse.js"
 export * from "./Types/HelperTypes.js";
 export * from "./constants.js";
 export * from "./rateLimiter/RateLimiter.js";
